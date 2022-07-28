791a5bab0bdc877c1ff7ef7e1d49a0a16ab7a404
kicchiHerakles <huseyin_kececi@theherakles.com>
2022-07-28 10:31:54 +0300

    kicchi sol

--- src/interview_tasks/array_tasks/S72_Array_WordBreakProblem.java
+++ src/interview_tasks/array_tasks/S72_Array_WordBreakProblem.java
@@ -31,25 +31,39 @@ public class S72_Array_WordBreakProblem {
 
     public static void main(String[] args) {
         //ArrayList<String> dictionary = new ArrayList<>(Arrays.asList("i", "like", "sam", "sung", "samsung", "mobile", "ice", "cream", "icecream", "man", "go", "mango"));
-        //segments(dictionary, "ilike");
-        //System.out.println(segments(dictionary, "ilikesamsung"));;
+        //System.out.println(kicchiSolution(dictionary, "ilike"));
+
+        //ArrayList<String> dictionary = new ArrayList<>(Arrays.asList("i", "like", "sam", "sung", "samsung", "mobile", "ice", "cream", "icecream", "man", "go", "mango"));
+        //System.out.println(defaultSolution(dictionary, "ilikesamsung"));
 
         //ArrayList<String> dictionary = new ArrayList<>(Arrays.asList("leet", "code"));
-        //System.out.println(segments(dictionary, "leetcode"));;
+        //System.out.println(defaultSolution(dictionary, "leetcode"));
 
         ArrayList<String> dictionary = new ArrayList<>(Arrays.asList("cats","dog","sand","and","cat"));
-        System.out.println(kicchiSolution(dictionary, "catsandog"));;
+        System.out.println(kicchiSolution(dictionary, "catsandog"));
+
+        //ArrayList<String> dictionary = new ArrayList<>(Arrays.asList("a","b","bbb","bbbb"));
+        //System.out.println(kicchiSolution(dictionary, "bb"));
+
+        //ArrayList<String> dictionary = new ArrayList<>(Arrays.asList("apple","pen"));
+        //System.out.println(kicchiSolution(dictionary, "applepenapple"));
+
+        //ArrayList<String> dictionary = new ArrayList<>(Arrays.asList("i", "like", "sam", "sung", "samsung", "mobile", "ice", "cream", "icecream", "man", "go", "mango"));
+        //System.out.println(defaultSolution(dictionary, "ilikesamsung"));
     }
 
-    public static boolean defaultSolution() {
-        String[] dictionary = {"i", "like", "sam", "sung", "samsung", "mobile", "ice", "cream", "icecream", "man", "go", "mango"};
-        String input = "ilikesamsung";
-
+    /**
+     * this code has a bug for these inputs
+     * "catsandog"
+     * ["cats","dog","sand","and","cat"]
+     * //expected false but returning true
+     */
+    public static boolean defaultSolution(ArrayList<String> dictionary, String input) {
         ArrayList<String> words = new ArrayList<>();
         for (int i = 0; i < input.length(); i++) {
             for (int j = i; j < input.length(); j++) {
                 String temp = input.substring(i, j+1);
-               if(Arrays.asList(dictionary).contains(temp) ){
+               if(dictionary.contains(temp) ){
                    if(!words.contains(temp)) {
                        words.add(temp);
                    }
@@ -60,22 +74,29 @@ public class S72_Array_WordBreakProblem {
        return words.size() > 0;
     }
 
+    private static ArrayList<ArrayList<String>> segments = new ArrayList<>();
+    private static String baseInput = "";
     //@kicchi not passing all tests on leetcode
     public static boolean kicchiSolution(List<String> dictionary, String input){
-        ArrayList<ArrayList<String>> segments = new ArrayList<>();
-        ArrayList<String> matches = new ArrayList<>();
+        baseInput = input;
+        getMatches(dictionary, input, new ArrayList<>());
+
+        return segments.size() != 0;
+    }
+
+    private static void getMatches(List<String> dictionary, String currentInput, ArrayList<String> matches){
         String currentPartOfInput = "";
         int remainingPartDeterminerIndex = 0;
 
         for (int i = 0; i < dictionary.size(); i++) {
             String currentDictionaryWord = dictionary.get(i);
-            if (remainingPartDeterminerIndex + currentDictionaryWord.length() <= input.length())
-                currentPartOfInput = input.substring(remainingPartDeterminerIndex, remainingPartDeterminerIndex + currentDictionaryWord.length());
+            if (remainingPartDeterminerIndex + currentDictionaryWord.length() <= currentInput.length())
+                currentPartOfInput = currentInput.substring(remainingPartDeterminerIndex, remainingPartDeterminerIndex + currentDictionaryWord.length());
             else{
-                if (!matches.isEmpty())
+                if (!matches.isEmpty() && matches.stream().mapToInt(s -> s.length()).sum() == baseInput.length())
                     segments.add(matches);
 
-                if (input.substring(0, remainingPartDeterminerIndex).endsWith(currentDictionaryWord)){
+                /*if (input.substring(0, remainingPartDeterminerIndex).endsWith(currentDictionaryWord)){
                     ArrayList<String> postMatch = new ArrayList<>();
                     for (String strFromLast: matches) {
                         if (!currentDictionaryWord.startsWith(strFromLast) && !currentDictionaryWord.endsWith(strFromLast)){
@@ -84,7 +105,7 @@ public class S72_Array_WordBreakProblem {
                     }
                     postMatch.add(currentDictionaryWord);
                     segments.add(postMatch);
-                }
+                }*/
                 break;
             }
 
@@ -92,12 +113,14 @@ public class S72_Array_WordBreakProblem {
                 matches.add(currentPartOfInput);
                 remainingPartDeterminerIndex += currentDictionaryWord.length();
             }else {
-                return false;
+
             }
         }
-        if (!matches.isEmpty())
-            segments.add(matches);
 
-        return segments.size() != 0;
+        String remainingInput = currentInput.substring(remainingPartDeterminerIndex);
+        if (!matches.isEmpty() && matches.stream().mapToInt(s -> s.length()).sum() == currentInput.length())
+            segments.add(matches);
+        else if (!remainingInput.isEmpty() && dictionary.stream().anyMatch(w -> remainingInput.contains(w)))
+            getMatches(dictionary, remainingInput, matches);
     }
 }
