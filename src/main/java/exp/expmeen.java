eb80dda3f63af8044f829e727c953c2f13207b82
ihccik <kicchi@cydeo.com>
2022-11-02 22:47:04 +0300

    week 28 added

---
+++ src/main/java/exp/expmeen.java
@@ -0,0 +1,12 @@
+package exp;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum expmeen {
+  ABA("x aba"), ZABA("x zaba");
+
+  private String x;
+}
